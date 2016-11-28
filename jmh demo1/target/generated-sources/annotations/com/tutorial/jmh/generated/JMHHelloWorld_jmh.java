17cdb87b83442dfd9f1c7de7961a019d1739c8d2
chetan <bairagisirfire@gmail.com>
2016-11-28 07:26:21 +0530

    first commit

---
+++ jmh demo1/target/generated-sources/annotations/com/tutorial/jmh/generated/JMHHelloWorld_jmh.java
@@ -0,0 +1,4 @@
+package com.tutorial.jmh.generated;
+public class JMHHelloWorld_jmh extends JMHHelloWorld_jmh_B3 {
+}
+
---
+++ jmh demo2/target/generated-sources/annotations/com/tutorial/jmh/generated/JMHSortBenchmark_jmh.java
@@ -0,0 +1,4 @@
+package com.tutorial.jmh.generated;
+public class JMHSortBenchmark_jmh extends JMHSortBenchmark_jmh_B3 {
+}
+
